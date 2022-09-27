e298099664e073f7bcd48993282d9b7e15edd6b0
Stefano Vesco <stefano.vesco@sidigroup.it>
2022-09-27 13:48:16 +0000

    Esempio app sapui5

---
+++ webapp/Component.js
@@ -0,0 +1,18 @@
+sap.ui.define(
+	[
+		"sap/ui/core/UIComponent"
+	],
+	function(UIComponent){
+		"use strict";
+		return UIComponent.extend("opensap.myapp.Component", {
+			metadata : {
+				manifest:"json"
+			},
+			init:function(){
+				UIComponent.prototype.init.apply(this, arguments);
+				
+				///this.getModel().setUseBatch(false);
+			}
+		});
+	}
+);
