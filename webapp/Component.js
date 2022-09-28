121f9e8010bc4856493ecb7da7d5261defefb921
Stefano Vesco <stefano.vesco@sidigroup.it>
2022-09-28 15:37:03 +0000

    Aggiunta descrizione per debug OData disabilitando use batch

--- webapp/Component.js
+++ webapp/Component.js
@@ -11,7 +11,12 @@ sap.ui.define(
 			init:function(){
 				UIComponent.prototype.init.apply(this, arguments);
 				
-				///this.getModel().setUseBatch(false);
+				// la riga sotto và scommentata solo se si vuole fare debug delle chiamate OData
+				// altrimenti fa una chiamata batch e non si vede cosa chiede ma un $batch
+				// disabilitando il batch le prestazioni crollano ma si vede cosa fa nella scheda Network selezionando ProductSet...:
+				// https://webidetesting9681828-a0fe0645a.dispatcher.hana.ondemand.com/destinations/ES5/sap/opu/odata/IWBEP/GWSAMPLE_BASIC/ProductSet?$skip=0&$top=5&$orderby=Category asc&$expand=ToSupplier
+				// nella scheda Response si vede come ha risposto il servizio OData
+				this.getModel().setUseBatch(false);
 			}
 		});
 	}
